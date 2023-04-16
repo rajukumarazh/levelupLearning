431fd3ce54868f56ee5516f9bd0b4f1506a61baf
rajukumarazh <rajukumarazh@gmail.com>
2023-04-16 10:02:49 +0530

    rechecking

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<p>Homepage 123</p>
+			<p>Homepage 13</p>
 		</main>
 	);
 }
