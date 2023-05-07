8ee2f2cafa4960b85ffa88dc871a06568c0d63f6
rajukumarazh <rajukumarazh@gmail.com>
2023-05-07 14:32:19 +0530

    QuizzTrivia Added

--- pages/course/CourseData.tsx
+++ pages/course/CourseData.tsx
@@ -1,46 +1,81 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import { useState } from "react";
+import q from "../../utils/q.json";
+import QuizzTrivia from "../course/QuizzTrivia";
 
 type Props = {
 	text: String;
 };
 
 export default function CourseData(Props) {
-	console.log('dkkkk', Props?.course[0]?.details);
+	const [courseData, setCourseData] = useState<string>("Quizz");
+	console.log("dkkkk", Props);
+	console.log("dkkkk", courseData);
 
 	return (
-		<div className="w-full rounded-md p-1 shadow-md shadow-indigo-200">
-			<div className="w-full flex  h-full">
-				<div className="w-2/3 pl-2">
-					{Props?.course[0]?.details == undefined ? (
-						<p>
-							{' '}
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum has been the industry's standard dummy text
-							ever since the 1500s, when an unknown printer took a galley of
-							type and scrambled it to make a type specimen book. It has
-							survived not only five centuries, but also the leap into
-							electronic typesetting, remaining essentially unchanged. It was
-							popularised in the 1960s with the release of Letraset sheets
-							containing Lorem Ipsum passages, and more recently with desktop
-							publishing software like Aldus PageMaker including versions of
-							Lorem Ipsum.
-						</p>
-					) : (
-						<p>{Props?.course[0]?.details}</p>
-					)}
+		<div className="w-full">
+			{courseData == "Quizz" ? (
+				// <div className="w-full rounded-md p-1 shadow-md shadow-indigo-200">
+
+				// </div>
+				<QuizzTrivia />
+			) : (
+				<div className="w-full rounded-md p-1 shadow-md shadow-indigo-200">
+					<div className="w-full flex  h-full">
+						<div className="w-2/3 pl-2">
+							{Props?.course[0]?.course[0]?.details ==
+							undefined ? (
+								<p>
+									Lorem Ipsum is simply dummy text of
+									the printing and typesetting
+									industry. Lorem Ipsum has been the
+									industry's standard dummy text ever
+									since the 1500s, when an unknown
+									printer took a galley of type and
+									scrambled it to make a type
+									specimen book. It has survived not
+									only five centuries, but also the
+									leap into electronic typesetting,
+									remaining essentially unchanged. It
+									was popularised in the 1960s with
+									the release of Letraset sheets
+									containing Lorem Ipsum passages,
+									and more recently with desktop
+									publishing software like Aldus
+									PageMaker including versions of
+									Lorem Ipsum.
+								</p>
+							) : (
+								<p>
+									{
+										Props?.course[0].course[0]
+											.details
+									}
+								</p>
+							)}
+						</div>
+						<div className="pl-4">
+							<img src="/code.png" alt="not found" />
+						</div>
+					</div>
+					<div className="flex justify-between p-1 mt-2">
+						<button className="w-12 rounded-md bg-blue-400 text-white hover:bg-blue-500 px-1 py-1 ">
+							Prev
+						</button>
+						<button className="w-12 rounded-md bg-blue-400 text-white hover:bg-blue-500 px-1 py-1 ">
+							Next
+						</button>
+					</div>
+
+					<button
+						onClick={() => setCourseData("Quizz")}
+						className="w-full rounded-md bg-blue-400 text-white hover:bg-blue-500 px-1 py-1 "
+					>
+						Attempt Test
+					</button>
 				</div>
-				<div className="pl-4">
-					<img src="/code.png" alt="not found" />
-				</div>
-			</div>
-			<div className="flex justify-between p-1 mt-2">
-				<button className="w-12 rounded-md bg-blue-400 text-white hover:bg-blue-500 px-1 py-1 ">
-					Prev
-				</button>
-				<button className="w-12 rounded-md bg-blue-400 text-white hover:bg-blue-500 px-1 py-1 ">
-					Next
-				</button>
-			</div>
+			)}
 		</div>
 	);
 }
---
+++ pages/course/QuizzTrivia.tsx
@@ -0,0 +1,166 @@
+import React from "react";
+import q from "../../utils/q.json";
+import { useEffect, useState } from "react";
+type Props = {};
+export default function QuizzTrivia() {
+	const [quest, setQuest] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalpage] = useState(quest?.length);
+	const [perPage, setperPage] = useState(1);
+	console.log("helloQuestion", quest);
+	useEffect(() => {
+		setQuest(() => q[0]?.geography);
+	}, [q]);
+	const totalNumberOfPage = totalpage / perPage;
+	let dt = Object.keys(q[0]);
+	let lastindex = currentPage * perPage;
+	let firstindex = lastindex - perPage;
+	let pages = quest?.slice(firstindex, lastindex);
+	console.log("pages", pages);
+	let count = [];
+	for (let i = 1; i <= totalNumberOfPage; i++) {
+		count.push(i);
+	}
+	const chooseAnswerFun = (ans, id) => {
+		let quiz = [];
+		quest?.forEach((e) => {
+			if (e.id === id) {
+				let qzz = { ...e, choosen: ans };
+				quiz.push(qzz);
+			}
+		});
+
+		console.log("id", id, ans, quiz);
+	};
+	console.log("cournt", count);
+	return (
+		<div className="w-full rounded-md p-1 shadow-md shadow-indigo-200 h-full">
+			<div className="flex justify-between">
+				{pages !== undefined ? (
+					pages?.map((curr, i, arr) => {
+						return (
+							<div>
+								{currentPage == quest?.length ? (
+									<p className="text-red-600  font-semibold underline text-center">
+										Last Question
+									</p>
+								) : (
+									""
+									// <p className="text-green-500 text-right text-underline">
+									// 	We think you doing great
+									// </p>
+								)}
+								<p className="text-red-500 font-semibold">
+									{curr?.id}.&nbsp;
+									{curr?.question}
+								</p>
+								<br />
+								{curr?.answers?.map((ans) => {
+									return (
+										<div className="flex items-center mb-4">
+											<input
+												id="default-radio-1"
+												type="radio"
+												onClick={() =>
+													chooseAnswerFun(
+														ans,
+														curr.id,
+													)
+												}
+												value={ans}
+												name="default-radio"
+												className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+											/>
+											<label
+												htmlFor="default-radio-1"
+												className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+											>
+												<p> {ans}</p>
+											</label>
+											{/* <button
+												className="bg-blue-500 text-white px-2 py-1"
+												onClick={() => chooseAnswerFun(ans, curr.id)}
+											>
+												{ans}
+											</button> */}
+										</div>
+									);
+								})}
+							</div>
+						);
+					})
+				) : (
+					<p>Question Not found</p>
+				)}
+				{/* <div className="p-5">
+					<select onChange={(e) => dispatch(chooseSubject(e.target.value))}>
+						{dt?.map((curr) => {
+							return <option value={curr}>{curr}</option>;
+						})}
+					</select>
+				</div> */}
+			</div>
+			<div className="flex justify-between">
+				<button
+					onClick={() =>
+						setCurrentPage(
+							currentPage !== 1
+								? currentPage - 1
+								: currentPage,
+						)
+					}
+					className="rounded-md px-2 py-1 text-black bg-green-500 shadow-md m-4"
+				>
+					Prev
+				</button>
+
+				<button
+					onClick={() =>
+						setCurrentPage(
+							currentPage !== quest.length
+								? currentPage + 1
+								: currentPage,
+						)
+					}
+					className="rounded-md px-2 py-1 text-black bg-green-500 shadow-md m-4"
+				>
+					Next
+				</button>
+			</div>
+			{count
+				? count.map((curr) => {
+						return (
+							<>
+								<button
+									value={curr}
+									className="rounded-md px-2 py-1 text-black bg-green-500 shadow-md m-4"
+									// onClick={(e) =>
+									// 	setCurrentPage(e.target.value)
+									// }
+								>
+									{curr}
+								</button>
+							</>
+						);
+				  })
+				: ""}
+			{/* {allState?.QNA?.current_course_quizz[
+				allState?.QNA?.current_course_quizz?.length - 1
+			].choosen !== undefined &&
+				allState?.QNA?.current_course_quizz?.filter(
+					(curr) => curr.choosen == undefined,
+				).length == 0 && ( */}
+			<button className="text-white font-semibold px-2 py-3 bg-red-600 absolute right-6 rounded-md mt-5">
+				Submit
+			</button>
+			{/* // )} */}
+			<button
+				// onClick={() => setCourseData("revision")}
+				className="w-24 rounded-md bg-blue-400 text-white hover:bg-blue-500 px-1 py-1 "
+			>
+				Revision
+			</button>
+			QuizzTrivia
+		</div>
+	);
+}
